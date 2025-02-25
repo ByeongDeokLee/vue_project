19bbbb2cac8a4fec6be977b83d08cc129ed6329b
ByeongDeokLee <bdlee6710@gmail.com>
2025-02-25 16:33:26 +0900

    버튼 증가 테스트

---
+++ src/components/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>카운트 : {{ count }}</h1>
+    <button @click="increaseCount">+1 증가</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyCounter",
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increaseCount() {
+      this.count++;
+    },
+  },
+};
+</script>
+
+<style>
+button {
+  padding: 10px;
+  font-size: 16px;
+  cursor: pointer;
+}
+</style>
